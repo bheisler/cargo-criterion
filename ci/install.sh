341ba411e3a3f18f7915def7b72ca2ed6695f19a
Brook Heisler <redattack34@gmail.com>
2020-06-28 09:53:09 -0600

    Add CI files.

---
+++ ci/install.sh
@@ -0,0 +1,14 @@
+set -ex
+
+if [ "$CLIPPY" = "yes" ]; then
+    rustup component add clippy-preview
+fi
+
+if [ "$RUSTFMT" = "yes" ]; then
+    rustup component add rustfmt
+fi
+
+if [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$GNUPLOT" = "yes" ]; then
+    brew unlink python@2 # because we're installing python3 and they both want to install stuff under /usr/local/Frameworks/Python.framework/
+    brew install gnuplot
+fi
---
+++ ci/script.sh
@@ -0,0 +1,26 @@
+set -ex
+
+if [ "$CLIPPY" = "yes" ]; then
+      cargo clippy --all -- -D warnings
+elif [ "$DOCS" = "yes" ]; then
+    cargo clean
+    cargo doc --all --no-deps
+    cd book
+    mdbook build
+    cd ..
+    cp -r book/book/html/ target/doc/book/
+    travis-cargo doc-upload || true
+elif [ "$RUSTFMT" = "yes" ]; then
+    cargo fmt --all -- --check
+elif [ "$MINIMAL_VERSIONS" = "yes" ]; then
+    rm Cargo.lock || true
+    cargo build -Z minimal-versions
+else
+    cargo check --no-default-features
+    cargo check --no-default-features --features gnuplot_backend
+    cargo check --no-default-features --features plotters_backend
+
+    cargo check --all-features
+
+    cargo test   
+fi
