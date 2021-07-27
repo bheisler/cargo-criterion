d0948eab1ea1fa12501ead84eaed0e93ce7d8297
David Himmelstrup <lemmih@gmail.com>
2021-07-27 10:30:45 +0800

    Set socket to nonblocking while accepting new connections.

--- src/bench_target.rs
+++ src/bench_target.rs
@@ -36,9 +36,12 @@ impl BenchTarget {
     ) -> Result<()> {
         let listener = TcpListener::bind("localhost:0")
             .context("Unable to open socket to connect to Criterion.rs")?;
-        //listener
-        //    .set_nonblocking(true)
-        //    .context("Unable to set socket to nonblocking")?;
+        // listener has to be non-blocking while we wait for connections.
+        // Otherwise we'll wait forever if no-one connects to us
+        listener
+            .set_nonblocking(true)
+            .context("Unable to set socket to nonblocking")?;
+        dbg!();
 
         let addr = listener
             .local_addr()
@@ -77,6 +80,9 @@ impl BenchTarget {
         loop {
             match listener.accept() {
                 Ok((socket, _)) => {
+                    socket
+                        .set_nonblocking(false)
+                        .context("Unable to set socket to blocking")?;
                     let conn = Connection::new(socket).with_context(|| {
                         format!("Unable to open connection to bench target {}", self.name)
                     })?;
